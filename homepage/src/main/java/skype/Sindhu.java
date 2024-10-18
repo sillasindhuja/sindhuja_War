ff2ee968152718f9311d5b70dbb46490fa56f8ec
sindhu <sindhusilla7@gmail.com>
2024-10-18 11:36:25 +0000

    warfile

---
+++ homepage/src/main/java/skype/Sindhu.java
@@ -0,0 +1,13 @@
+package skype;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sindhu
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello SindhujaWorld!" );
+    }
+}
